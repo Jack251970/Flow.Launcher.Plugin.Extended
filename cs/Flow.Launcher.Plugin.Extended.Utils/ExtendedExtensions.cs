e91ec50fa9a66150ff9cea11f204b434b3db2f4b
Jack251970 <1160210343@qq.com>
2025-03-03 10:56:54 +0800

    feature: add extended extensions

---
+++ cs/Flow.Launcher.Plugin.Extended.Utils/ExtendedExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using JetBrains.Annotations;
+
+namespace Flow.Launcher.Plugin.Extended.Utils;
+
+[PublicAPI]
+public static class ExtendedExtensions
+{
+    /// <summary>
+    /// <see cref="IPublicAPI"/> instance.
+    /// </summary>
+    private static IPublicAPI? _api;
+
+    /// <summary>
+    /// Get <see cref="IPublicAPI"/> instance even in plugin constructors.
+    /// </summary>
+    /// <remarks>
+    /// From <see cref="Flow.Launcher.Plugin"/> 4.5.0, we can use <see cref="Ioc"/> to get <see cref="IPublicAPI"/> instance.
+    /// </remarks>
+    public static IPublicAPI Api => _api ??= Ioc.Default.GetRequiredService<IPublicAPI>();
+}
